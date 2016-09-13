b93299694eb2631e1ee31bfe8065d945a686dd97
mrn24 <mrn24@nau.edu>
2016-09-13 15:53:30 -0700

    Read all the header files

--- p0.c
+++ p0.c
@@ -4,19 +4,61 @@
 #include <stdlib.h>
 
 
-typedef struct RGBApixel {
-	unsigned char r, g, b, a;
-} RGBApixel;
+typedef struct RGBpixel {
+	unsigned char r, g, b;
+} RGBpixel;
 
+typedef struct Image{
+  int width, height, format, range;
+  RGBpixel *buffer;
+}Image;
 
-int main(argv[], argc){
-  if(argc < 2){
-    return 0;
-  }
+Image image;
+
+int main(int argc, char **argv){
+
+  //if(argc < 2){
+  //  return 0;
+  //}
   char mn_buffer[8];
   char buffer[100];
-  FILE* fp = fopen(argv[1], "r");
-  
 
+  int i = read_image("./SamplePPMAscii.ppm");
+  printf("%d\n", i);
   return 1;
+}
+
+int read_image(char* input){
+  FILE* fp = fopen(input, "r");
+  int c;
+  c = fgetc(fp);
+  printf("%c\n", c);
+  if(c!= 'P'){
+    printf("Not a PPM File\n");
+    return 0;
+  }else if(c == 'P'){
+    printf("Is a PPM File\n");
+  }else{
+    printf("I dunno");
+    return 0;
+  }
+  c = fgetc(fp);
+  image.format = c;
+  c = fgetc(fp);
+  c = fgetc(fp);
+  printf("%c\n", c);
+
+  while (c == '#'){
+    while(c != '\n'){
+      c = fgetc(fp);
+    }
+  }
+  while(c == ' '){
+    c = fgetc(fp);
+  }
+  //c = fgetc(fp);
+  fscanf(fp, "%d %d %d", &image.width, &image.height, &image.range);
+  printf("%d - %d - %d\n", image.width, image.height, image.range);
+  
+  return 0;
 }
